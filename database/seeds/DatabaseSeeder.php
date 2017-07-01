8dcae1f822131f513f6c79a24ee491828c3df4af
workstation\Halim <halim.webdev@gmail.com>
2017-07-01 18:12:21 -0400

    create table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder {
      */
     public function run() {
         $this->call(UsersTableSeeder::class);
+        //$this->call(AuthorTableSeeder::class);
+        //$this->call(LanguagesTableSeeder::class);
+        //$this->call(BookTableSeeder::class);
     }
 
 }
